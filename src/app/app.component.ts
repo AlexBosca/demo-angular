7982ecda43c094280b61dd395bd4ed432fe79ced
AlexBosca <alex_241998@yahoo.com>
2026-02-02 22:20:21 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,4 +10,20 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'demo-angular';
+
+  constructor(private http: HttpClient) {}
+
+  getUsers() {
+    return this.http.get('https://demo-iwmn.onrender.com/demo').subscribe((data) => {
+      console.log(data);
+    });
+  }
+
+  createUser() {
+    const user = { "id": 100, "name": 'Another User' };
+
+    return this.http.post('https://demo-iwmn.onrender.com/demo', user).subscribe((data) => {
+      console.log(data);
+    });
+  }
 }
