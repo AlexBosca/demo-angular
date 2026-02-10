b3734ea60225e917092ebf31548f346468a5f5e5
AlexBosca <alex_241998@yahoo.com>
2026-02-10 22:25:23 +0200

    Display list of users fetched from API in AppComponent

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
@@ -5,26 +6,29 @@ import { environment } from '../environments/environment.prod';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'demo-angular';
 
+  users: any[] = [];
+
   constructor(private http: HttpClient) {}
 
   getUsers() {
     return this.http.get(`${environment.apiUrl}/demo`).subscribe((data) => {
       console.log(data);
+      this.users = data as any[];
     });
   }
 
-  createUser() {
-    const user = { "name": 'Another User' };
+  // createUser() {
+  //   const user = { "name": 'Another User' };
 
-    return this.http.post(`${environment.apiUrl}/demo`, user).subscribe((data) => {
-      console.log(data);
-    });
-  }
+  //   return this.http.post(`${environment.apiUrl}/demo`, user).subscribe((data) => {
+  //     console.log(data);
+  //   });
+  // }
 }
